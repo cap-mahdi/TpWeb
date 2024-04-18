2c35f19ecd31d19bd40a3aa9df1c22b4da1a82db
Med Amine Gdoura <medaminegdoura@gmail.com>
2024-04-18 10:49:00 +0100

    Seeding db+  context

---
+++ graphql/src/db/db.ts
@@ -0,0 +1,43 @@
+export const db = {
+  users: [
+    {
+      id: 1,
+      email: "mehdi@fekih.com",
+      name: "Mehdi fkih",
+      role: "admin",
+    },
+    {
+      id: 2,
+      email: "medamine@gdoura.com",
+      name: "Med Amine Gdoura",
+      role: "admin",
+    },
+    {
+      id: 3,
+      email: "Mahdi@chaabane.com",
+      name: "Mahdi Chaabane",
+      role: "user",
+    },
+    {
+      id: 4,
+      email: "mehdi@khelil.com",
+      name: "Mehdi khelil",
+      role: "user",
+    },
+    {
+      id: 5,
+      email: "houssem@sahnoun.com",
+      name: "Houssem Sahnoun",
+      role: "admin",
+    },
+  ],
+  skills: [
+    { id: 1, designation: "HTML" },
+    { id: 2, designation: "CSS" },
+    { id: 3, designation: "JavaScript" },
+    { id: 4, designation: "React" },
+    { id: 5, designation: "Node.js" },
+    { id: 6, designation: "Python" },
+    { id: 7, designation: "SQL" },
+  ],
+};
