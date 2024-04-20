02cd7c1cd5b56845215380b40aa3238ec0eb5b1b
Mahdi Chaabane <mahdi02ch@gmail.com>
2024-04-20 01:54:27 +0100

    merge with main

--- graphql/src/context.ts
+++ graphql/src/context.ts
@@ -1,11 +1,11 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export type GraphQLContext = {
-    prisma: PrismaClient
-}
+  prisma: PrismaClient;
+};
 
 export function createContext(): GraphQLContext {
-    return { prisma }
+  return { prisma };
 }
