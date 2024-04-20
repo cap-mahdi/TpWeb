51a8704fcdf0525aaf6843fbe6e9168ed871a204
Mahdi Chaabane <mahdi02ch@gmail.com>
2024-04-20 10:24:32 +0100

    fix query

--- graphql/src/resolvers/query.ts
+++ graphql/src/resolvers/query.ts
@@ -1,5 +1,5 @@
 import { GraphQLError } from "graphql";
-import { Context, Cv } from "../types";
+import { Context, Cv, CvWithSkills } from "../types";
 import { cvExists } from "../utils";
 
 type GetCvByIdArgs = {
@@ -7,13 +7,34 @@ type GetCvByIdArgs = {
 };
 
 export const Query = {
-  getAllCv: (_: unknown, _1: {}, { db }: Context): Cv[] => {
-    return db.cvs;
+  getAllCv: (_: unknown, _1: {}, { db }: Context): CvWithSkills[] => {
+    const cvs = db.cvs;
+    return cvs.map((cv: Cv) => ({
+      ...cv,
+      skills: db.skills.reduce((acc, skill) => {
+        if (
+          db.cvSkills.find(
+            (cvSkill) => cvSkill.cvId === cv.id && cvSkill.skillId === skill.id
+          )
+        ) {
+          acc.push(skill.id);
+        }
+        return acc;
+      }, [] as string[]),
+    }));
   },
-  getCvByID: (_: unknown, { id }: GetCvByIdArgs, { db }: Context): Cv => {
+  getCvByID: (
+    _: unknown,
+    { id }: GetCvByIdArgs,
+    { db }: Context
+  ): CvWithSkills => {
     if (!cvExists(db, id)) {
       throw new GraphQLError(`Cv with id ${id} not found`);
     }
-    return db.cvs.find((cv: Cv) => cv.id === id)!;
+
+    const cv = db.cvs.find((cv: Cv) => cv.id === id)!;
+    const skills = db.cvSkills.filter((cvSkill) => cvSkill.cvId === id);
+
+    return { ...cv, skills: skills.map((cvSkill) => cvSkill.skillId) };
   },
 };
